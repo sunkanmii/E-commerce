57f7258c541bb627473f09c72168cbc142ff90ca
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2020-03-14 23:08:48 +0100

    I did a whole bunch of stuff I'm still trying to understand

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,18 +1,44 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 
 import { throwError } from 'rxjs';
-import { retry, catchError } from 'rxjs/operators';
+import { retry, catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 
+
 export class ApiService {
   private SERVER_URL = 'http://localhost:3000';
 
+  public first = '';
+  public prev = '';
+  public next = '';
+  public last = '';
+
   constructor(private httpClient: HttpClient) {
 
+  }
+
+  parseLinkHeader(header) {
+    if (header.length === 0) {
+      return;
+    }
+
+    let parts = header.split(',');
+    var links = {};
+    parts.forEach(p => {
+      let section = p.split(';');
+      var url = section[0].replace(/<(.*)>/, '$1').trim();
+      var name = section[1].replace(/rel="(.*)"/, '$1').trim();
+      links[name] = url;
+    });
+
+    this.first = links['first'];
+    this.last = links['last'];
+    this.prev = links['prev'];
+    this.next = links['next'];
   }
 
   handleError(error: HttpErrorResponse) {
@@ -27,7 +53,19 @@ export class ApiService {
   }
 
   public sendGetRequest() {
-    return this.httpClient.get(this.SERVER_URL).pipe(catchError(this.handleError));
+  // tslint:disable-next-line: max-line-length
+  return this.httpClient.get(this.SERVER_URL, {  params: new HttpParams({fromString: "_page=1&_limit=20"}), observe: "response"}).pipe(retry(3), catchError(this.handleError), tap(res => {
+      console.log(res.headers.get('Link'));
+      this.parseLinkHeader(res.headers.get('Link'));
+    }));
+  }
+
+  public sendGetRequestToURL(url: string) {
+    return this.httpClient.get(url, {observe: 'response'}).pipe(retry(3),
+    catchError(this.handleError), tap(res => {
+      console.log(res.headers.get('Link'));
+      this.parseLinkHeader(res.headers.get('Link'));
+    }));
   }
 
   public get() {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { HttpResponse } from '@angular/common/http';
+import { takeUntil } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
   products = [];
+  destroy$: Observable<any>;
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
-    this.apiService.get().subscribe((data: any[]) => {
-      console.log(data);
-      this.products = data;
+    this.apiService.sendGetRequest().pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.products = res.body;
     });
   }
 
+  public firstPage(){
+    this.products = [];
+    this.apiService.sendGetRequestToURL(this.apiService.first).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.products = res.body;
+    });
+  }
+
+  public prevPage(){
+    if(this.apiService.prev !== undefined && this.apiService.prev !== ''){
+      this.products = [];
+      this.apiService.sendGetRequestToURL(this.apiService.prev).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.products = res.body;
+    });
+    }
+  }
+
+  public nextPage(){
+    if(this.apiService.next !== undefined && this.apiService.next !== ''){
+      this.products = [];
+      this.apiService.sendGetRequestToURL(this.apiService.next).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.products = res.body;
+    });
+    }
+  }
+
+  public lastPage(){
+    this.products = [];
+    this.apiService.sendGetRequestToURL(this.apiService.last).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.products = res.body;
+    });
+  }
 }
